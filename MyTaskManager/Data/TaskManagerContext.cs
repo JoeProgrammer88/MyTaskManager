dd0b5d7b9192360ca08843f6941e5ed2732070c8
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-03-24 14:51:09 -0700

    Add EF Core DbContext

---
+++ MyTaskManager/Data/TaskManagerContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTaskManager.Models;
+
+namespace MyTaskManager.Data
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions<TaskManagerContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite($"Data Source=MyTaskManager.db;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ToDoItem>(todo =>
+            {
+                todo.HasKey(x => x.Id);                         // PK
+                todo.Property(x => x.Id).ValueGeneratedOnAdd(); // Identity column
+            });
+        }
+
+        public DbSet<ToDoItem> ToDos { get; set; }
+    }
+}
